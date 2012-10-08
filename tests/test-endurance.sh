c5daf44996391cb48117ce7f1d11bad6209668ed
Tommi Rantala <tt.rantala@gmail.com>
2012-10-08 12:16:12 +0300

    Use new program names in tests/test-endurance.sh

--- tests/test-endurance.sh
+++ tests/test-endurance.sh
@@ -9,12 +9,12 @@ exit_cleanup ()
 trap exit_cleanup EXIT
 
 # create data snapshots with sp-endurance tools
-save-incremental-endurance-stats $tmpdir
-save-incremental-endurance-stats $tmpdir
-save-incremental-endurance-stats $tmpdir
+endurance-snapshot $tmpdir
+endurance-snapshot $tmpdir
+endurance-snapshot $tmpdir
 
 # parse the snaphosts with sp-endurance-postproc tools
-parse-endurance-measurements --report=$report $tmpdir/1*
+endurance-parse-snapshots --report=$report $tmpdir/[0-9][0-9][0-9]
 
 # HTML report generation finished?
 fgrep -q '</body>' $report.html
